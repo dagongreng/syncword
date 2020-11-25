43ccd67bd57d95b3ba9ce913ce68796e8062a173
dagongreng <35368543@qq.com>
2020-11-25 16:33:22 +0800

    require-directory

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
-const Koa = require('Koa')
-const app = new Koa()
-
+const app = require('./koa')
 const conf = require('./config')
 
-app.use(ctx=>{
-    ctx.body ='hello koa2'
+// 当我们配制 ejs 模板引擎的时候
+// 配制的代码一定要放在router 配制代码之前 
+// 这个比较容易被忽略
+
+const router = require('./router')
+
+
+app.use(ctx => {
+  ctx.body = 'hello Syncword'
 })
 
-app.listen(conf.server.PORT,()=>{
-    console.log(`Server run at http://localhost:${conf.server.PORT}`)
+app.listen(conf.server.PORT, () => {
+  console.log(`Server run at http://localhost:${conf.server.PORT}`)
 })
---
+++ router/routes/user.js
@@ -0,0 +1,17 @@
+const KoaRouter = require('koa-router')
+const router = new KoaRouter()
+
+
+//register
+router.post('/register',async ctx =>{
+    ctx.body = `register api`
+})
+//login
+router.get('/login', async ctx => {
+    ctx.body = `login api`
+})
+//logout
+router.get('/logout', async ctx => {
+    ctx.body = `logout api`
+})
+module.exports = router
